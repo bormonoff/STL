87e5bb1c6a80076a6e706f5860f82f96876e51c9
Bormon <bormon_off@protonmail.com>
2023-04-13 20:36:24 +0300

    Add basic vector functions

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <tr>
 <td>
 
-Resently I was asked to `write my own Vector`(analog of the List in the C# or ArrayList in the JAVA). I seemed interesting to me cause several years ago I 
+Recently I was asked to `write my own Vector`(analog of the List in the C# or ArrayList in the JAVA). It seemed interesting to me cause several years ago I 
 already wrote some STL classes but my implementation was quite primitive. So I decided to write again some STL classes like Binary or B-trees, Hash-table (_Hello ~~World~~ Google_), Variant & Any.
  </td>
  </tr>
--- src/vector.h
+++ src/vector.h
@@ -7,34 +7,36 @@ template <typename Type, typename Alloc = std::allocator<Type>>
 class vector {
 public:
     using Alloc_traits = std::allocator_traits<Alloc>;
+    using iterator = Type*;
 
     vector() noexcept(noexcept(Alloc())) = default;
 
     // STL vector checks the max possible size via alloc::max_size
     // I will not check
     // ? Constexpr ? 
+
     explicit vector(size_t count, const Alloc& alloc = Alloc()) 
         : size_{count}, capacity_{count}, allocator_{alloc} {
-        initialize(capacity_);
+        ptr_ = Alloc_traits::allocate(allocator_, capacity_); 
         fill_with_copy(count);
     }
 
     explicit vector(size_t count, Type& T, const Alloc& alloc = Alloc())
         : size_{count}, capacity_{count}, allocator_{alloc} {
-        initialize(capacity_);
+        ptr_ = Alloc_traits::allocate(allocator_, capacity_); 
         fill_with_copy(count, T);
     }
 
     template<typename InputIterator>
     vector(InputIterator first, InputIterator second, const Alloc& alloc = Alloc())
         : size_{second - first}, capacity_{size_}, allocator_{alloc} {
-        initialize(capacity_);
+        ptr_ = Alloc_traits::allocate(allocator_, capacity_); 
         fill_in_range(first, second);
     }
 
     vector(const vector& other) 
         : size_{other.size_}, capacity_{other.capacity_}, allocator_{other.allocator} {
-        initialize(capacity_);
+        ptr_ = Alloc_traits::allocate(allocator_, capacity_); 
         fill_in_range(other.begin(), other.end());        
     }
 
@@ -51,7 +53,7 @@ public:
         if (capacity_ < other.capacity_) {
             destroy();
             deallocate;
-            initialize(other.capacity_);
+            ptr_ = Alloc_traits::allocate(allocator_, other.capacity_); 
         } else {
             destroy();
         }
@@ -67,6 +69,115 @@ public:
         other.ptr_ = nullptr;
         other.size_ = 0;
         other.capacity_ = 0;
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args) {
+        if (size_ == capacity_) {
+            reserve(capacity_ * 2);
+        }
+        Alloc_traits::construct(allocator_, ptr_ + size_, std::forward<Args>(args)...);
+        ++size_;
+    }
+
+    // Push_back overload for l & r -values
+    void push_back(const Type& value) {
+        if (size_ == capacity_) {
+            reserve(capacity_ * 2);
+        }
+        Alloc_traits::construct(allocator_, ptr_ + size_, value);
+        ++size_;
+    }
+
+    void push_back(Type&& value) {
+        if (size_ == capacity_) {
+            reserve(capacity_ * 2);
+        }
+        Alloc_traits::construct(allocator_, ptr_ + size_, std::move(value));
+        ++size_;
+    }
+
+
+    void pop_back() {
+        Alloc_traits::destroy(allocator_, ptr_ + size_);
+        --size_;
+    }
+    
+    void reserve(size_t size) {
+        if (size <= capacity_) { return; } 
+        Type* newptr = Alloc_traits::allocate(allocator_, size); 
+        size_t i{0};
+        try {
+            for(; i < size; ++i) {
+                Alloc_traits::construct(allocator_, newptr + i, std::move_if_noexcept(ptr_ + i));
+            }
+        } catch(...) {
+            for(int j = 0; j < i; ++j) {
+                Alloc_traits::destroy(allocator_, newptr[j]);
+            }
+            Alloc_traits::deallocate(allocator_, newptr, i);
+            return;
+        }
+        destroy();
+        Alloc_traits::deallocate(allocator_, ptr_, capacity_); 
+        ptr_ = newptr;
+        capacity_ = size;
+    }
+
+    // Type must be default constructable or have copy constructor
+    void resize(size_t size, const Type& value = Type()) {
+        if (size == capacity_) { return; }
+        Type* newptr = Alloc_traits::allocate(allocator_, size); 
+        size_t i{0};
+        size_t max{0};
+        try {
+            (size < size_) ? (max = size) : (max = size_);
+            for(; i < max; ++i) {
+                Alloc_traits::construct(allocator_, newptr + i, std::move_if_noexcept(ptr_ + i));
+            }
+        } catch(...) {
+            for(int j = 0; j < max; ++j) {
+                Alloc_traits::destroy(allocator_, newptr[j]);
+            }
+            Alloc_traits::deallocate(allocator_, newptr, i);
+            return;
+        }
+        if (size < size_) {
+            destroy();
+            Alloc_traits::deallocate(allocator_, ptr_, capacity_); 
+            ptr_ = newptr;
+            capacity_ = size;
+            size_ = size;   
+        } else {
+            for (; i < size; ++i) {
+                Alloc_traits::construct(allocator_, newptr + i, value);
+            }
+            destroy();
+            Alloc_traits::deallocate(allocator_, ptr_, capacity_); 
+            ptr_ = newptr;
+            capacity_ = size;
+            size_ = size;
+        }
+    }
+
+    void shrink_to_fit() {
+        Type* newptr = Alloc_traits::allocate(allocator_, size_); 
+        size_t i{0};
+        try {
+            for(; i < size; ++i) {
+                Alloc_traits::construct(allocator_, newptr + i, std::move_if_noexcept(ptr_ + i));
+            }
+        } catch(...) {
+            for(int j = 0; j < i; ++j) {
+                Alloc_traits::destroy(allocator_, newptr[j]);
+            }
+            Alloc_traits::deallocate(allocator_, newptr, i);
+            return;
+        }
+        destroy();
+        Alloc_traits::deallocate(allocator_, ptr_, capacity_); 
+        ptr_ = newptr;
+        capacity_ = size_;
     }
 
     Type& at(size_t position) {
@@ -103,39 +214,43 @@ private:
     template<typename InputIterator>
     void fill_in_range(InputIterator first, InputIterator second) {
         size_t count = second - first;
+        size_t i{0};
         try {
-            for (int i = 0; i < count; ++i) {
+            for (; i < count; ++i) {
                 Alloc_traits::construct(allocator_, ptr_ + i, *first++);
             }
         } catch(...) {
+            for (int j = 0; j < i; ++j) {
+                Alloc_traits::destroy(allocator_, ptr_ + j); 
+            }
             Alloc_traits::deallocate(allocator_, ptr_, count);
             throw;
         }
     }
 
     void fill_with_copy(size_t count, const Type& value = Type()) {
+        size_t i{0};
         try {
-            for (int i = 0; i < count; ++i) {
+            for (; i < count; ++i) {
                 Alloc_traits::construct(allocator_, ptr_ + i, value);
             }
         } catch(...) {
+            for (int j = 0; j < i; ++j) {
+                Alloc_traits::destroy(allocator_, ptr_ + j); 
+            }
             Alloc_traits::deallocate(allocator_, ptr_, count);
             throw;
         }
     }
 
-    void initialize(size_t capacity) {
-        ptr_ = Alloc_traits::allocate(allocator_, capacity); 
-    }
-
     void destroy() {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size_; ++i) {
             Alloc_traits::destroy(allocator_, ptr_ + i);
         }
     }
     
     void deallocate() {
-        Alloc_traits::deallocate(allocator_, ptr_, capacity); 
+        Alloc_traits::deallocate(allocator_, ptr_, capacity_); 
         size_ = 0;
         capacity_ = 0;
         ptr_ = nullptr;
